8b66330f130126172d8e23a56e6ed852c2d87d9e
xKt4Vze <40662501+xKt4Vze@users.noreply.github.com>
2018-06-28 15:48:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -100,5 +100,27 @@ if(cmd === `${prefix}kick`){
 
   return;
 }
+  if(cmd === `${prefix}unban`){
+    const reason = args.slice(1).join(' ');
+    const user = args[0];
+    if (!user) return message.channel.send("You need Specific User ID").catch(console.error);
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("אין לך גישה לפקודה זו");
+    message.guild.unban(user);
+
+    message.delete().catch(O_o=>{});
+
+    let unbanEmbed = new Discord.RichEmbed()
+    .setDescription("**UnBan**")
+    .setColor("RANDOM")
+    .addField("**User**", `<@!`+user+`>`)
+    .addField("**Staff**", `<@${message.author.id}>`);
+
+    let UnBanchannel = message.guild.channels.find(`name`, "logs");
+    if(!UnBanchannel) return message.channel.send("Can't find channel called `logs`");
+
+    UnBanchannel.send(unbanEmbed);
+    message.channel.send(`***${member.user.username}#${member.user.discriminator} has been unbanned***`);
+
+}
 });
 client.login(process.env.BOT_TOKEN);
